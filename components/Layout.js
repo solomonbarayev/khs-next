734d97b9a5f87aef5c4bfd3230cbee556f2d0739
solomonbarayev <sbarayev2@gmail.com>
2022-12-21 19:18:25 +0200

    port mobile nav to next from react

--- components/Layout.js
+++ components/Layout.js
@@ -2,8 +2,13 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import styled from 'styled-components';
 import { primaryFont } from '../utils/style-variables';
+import { BiMenuAltRight } from 'react-icons/bi';
+import { useState } from 'react';
+import Drawer from './Drawer';
 
 const Layout = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <Wrapper>
       <div className="app__layout-wrapper">
@@ -15,6 +20,13 @@ const Layout = ({ children }) => {
           <Footer />
         </div>
       </div>
+
+      <HamburgerContainer onClick={() => setIsOpen(!isOpen)}>
+        <BiMenuAltRight />
+      </HamburgerContainer>
+      <Drawer isOpen={isOpen} setIsOpen={setIsOpen}>
+        <Navbar type="mobile" setIsOpen={setIsOpen} />
+      </Drawer>
     </Wrapper>
   );
 };
@@ -53,5 +65,19 @@ const Wrapper = styled.div`
     .app__layout-sidebar {
       height: 0;
     }
+  }
+`;
+
+const HamburgerContainer = styled.div`
+  display: none;
+  @media (max-width: 1089px) {
+    display: block;
+    position: absolute;
+    z-index: 99999;
+    top: 20px;
+    right: 20px;
+    font-size: 3rem;
+    color: #0c1670;
+    cursor: pointer;
   }
 `;
